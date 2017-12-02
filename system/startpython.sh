41685ca4bcffffc20ad2fe97b8b4d1df8cbfea0e
simat <simat@debian.karrak.org>
2017-12-02 18:45:03 +0800

    modify start script

--- system/startpython.sh
+++ system/startpython.sh
@@ -1,6 +1,5 @@
 TZ='Australia/Perth'; export TZ
 cd ~/BatteryMonitor
-until [ $? == 9 ]; do
-  python -u batteries.py 1> /dev/null 2>~/batteries.log
-done
+python -u batteries.py $1 1> /dev/null 2>batteries.log
+# python batteries.py
 exit 0
