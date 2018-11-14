cae25b8f4afc6bd1da3f159f73fb3fc66e635b2d
Simon Matthews <s.matthews@karrak.id.au>
2018-11-14 11:07:59 +0800

    Add code for 1 hour battery power average

--- html/batday.php
+++ html/batday.php
@@ -150,6 +150,7 @@ $amps = getdat("ioutmax");
 $dod = getdat("dod");
 $temp = getdat("tmax");
 $state = getdat("state");
+$batpwr1hrav = getdat("batpwr1hrav")
 $capacity = round(100*($batcapacity-$dod[0])/$batcapacity);
 if  ($capacity > "75") {
    $capcolour="green";
@@ -286,6 +287,7 @@ echo "<p><b>" . (date("l jS \of F Y h:i:s A", strtotime($timestamp)) . "</b></p>
 <?php echo $capacity."%"; ?></p>
 <p style="font-size:1.5em;line-height:0.0em;color:LightGray"><?php echo $amps[count($amps)-3]."A"?></p>
 <p style="font-size:0.5em;line-height:0.0;"><?php echo "In ".$amps[count($amps)-2]."A Out ".$amps[count($amps)-1]."A"?></p>
+<p style="font-size:0.5em;line-height:0.0;"><?php echo "1Hr Battery Power Av ".$1hrbatpwrav."kW"?></p>
 <p style="font-size:0.5em;line-height:1.0em;"><?php foreach ($state as $value) {
   echo "Charge State $value <br>";
 }
