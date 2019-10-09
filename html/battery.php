0fd0ef055492741908951e8abf7de565a523e867
Simon Matthews <s.matthews@karrak.id.au>
2019-10-09 16:48:34 +0800

    use config file to find summary file

--- html/battery.php
+++ html/battery.php
@@ -5,7 +5,13 @@
 <h1>Battery Stats</h1>
 <?php
 //Replace location of battery config file with the location on your system
-$myfile = fopen("/media/sdcard/summary", "r") or die("Unable to open file!");
+$pos = strpos($config,"summary");
+$pos = strpos($config,"'",$pos);
+$summary = substr($config,$pos+1);
+$summary = strstr($summary,"'",true);
+$myfile = fopen($summary,"r") or die("Unable to open file!");
+
+// $myfile = fopen("/media/sdcard/summary", "r") or die("Unable to open file!");
 // Output one line until end-of-file
 while(!feof($myfile)) {
   echo fgets($myfile) . "<br>";
