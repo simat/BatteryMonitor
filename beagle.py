d9641ca77261ac0792443083c1a7a57f4ce66a37
Simon Matthews <s.matthews@karrak.id.au>
2020-11-27 19:23:08 +0800

    Add beaglebone interface and buggy config file

---
+++ beagle.py
@@ -0,0 +1,32 @@
+
+#!/usr/bin/python
+# *****BatteryMonitor Getdata from battery cells getdata.py*****
+# Copyright (C) 2020 Simon Richard Matthews
+# Project loaction https://github.com/simat/BatteryMonitor
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import Adafruit_BBIO.GPIO as gpio
+import Adafruit_BBIO.ADC as adc
+adc.setup()
+
+class Rawdat():
+  """class for using the raspberry Pi for IO"""
+
+  def __init__(self):
+gpio.setup("P9_41", GPIO.OUT)
+adc.setup()
+self.rawdat={'ADCP9_39':0.0}
+
+  def getdata(self):
+    self.rawdat['ADCP9_39'] = adc.read("P9_39")
+    print self.rawat['ADCP_39']
