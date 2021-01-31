fa80bf72442deee6eb2b3691c6583c632eb2c0b5
Simon Matthews <s.matthews@karrak.id.au>
2021-01-31 12:48:36 +0800

    retry sending BMS command if no relpy or error

--- bms.py
+++ bms.py
@@ -62,7 +62,7 @@ class Bms:
     return crc
 
   def openbms(self,port):
-    self.port = serial.Serial(port,timeout=5)  # open serial port
+    self.port = serial.Serial(port,timeout=1)  # open serial port
 
   def getbmsdat(self,port,command):
     """ Issue BMS command and return data as byte data """
@@ -76,22 +76,31 @@ class Bms:
 
 
   def sendbms(self,port,packet):
-    """Send complete command string with crc to BMS"""
-    self.port.flushInput()
-    self.port.write(packet)
-    reply = self.port.read(4)
-#        raise serial.serialutil.SerialException('hithere')
+        """Send complete command string with crc to BMS"""
+    try:
+      for i in range(3):
+      self.port.flushInput()
+      self.port.write(packet)
+      reply = self.port.read(4)
+  #        raise serial.serialutil.SerialException('hithere')
 
-#    print (reply)
-    x = int.from_bytes(reply[3:5], byteorder = 'big')
-#    print (x)
-    data = self.port.read(x)
-    end = self.port.read(3)
-#    print (data,end,self.crccalc(reply[2:4]+data),end[0:2])
-    if self.crccalc(reply[2:4]+data)!=int.from_bytes(end[0:2],byteorder='big'):
-      raise serial.serialutil.SerialException('CRC data= {} calCRC={} CRC={}'.format(data,self.crccalc(reply[2:4]+data),int.from_bytes(end[0:2],byteorder='big')))
-#    print (data)
+  #    print (reply)
+      x = int.from_bytes(reply[3:5], byteorder = 'big')
+  #    print (x)
+      data = self.port.read(x)
+      end = self.port.read(3)
+  #    print (data,end,self.crccalc(reply[2:4]+data),end[0:2])
+      if data = b'':
+        raise serial.serialutil.SerialException('No Reply')
 
+      if self.crccalc(reply[2:4]+data)!=int.from_bytes(end[0:2],byteorder='big'):
+        raise serial.serialutil.SerialException('CRC data= {} calCRC={} CRC={}'.format(data,self.crccalc(reply[2:4]+data),int.from_bytes(end[0:2],byteorder='big')))
+  #    print (data)
+    except serial.serialutil.SerialException:
+      if i=2:
+        raise
+      else:
+        pass
 
     return data
 
