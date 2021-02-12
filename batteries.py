1f4f73be42e2c44b6db9aaf56221e43d365247c9
Simon Matthews <s.matthews@karrak.id.au>
2021-02-12 13:54:55 +0800

    Improve error handling, try to reintialise and restart several times before abort

--- batteries.py
+++ batteries.py
@@ -30,131 +30,139 @@ log.addHandler(logger.errfile)
 from demandmanager import solaravailable
 from getdata import Readings
 from alarms import Alarms
+maxtries=3 # number of error retries befor abort
 #exec(config['files']['alarms'])  # import alarm code
 #import Adafruit_BBIO.GPIO as GPIO
 
+def initmain(soc):
+  """ initialise main loop, soc is SOC to start battery DOD to"""
+  printtime = time.strftime("%Y%m%d%H%M%S ", time.localtime())
+  while int(printtime) <= int(summary['current']['timestamp']):
+    print(printtime,summary['current']['timestamp'])
+    print ("Error: Current time before last sample time")
+    time.sleep(30)
+    printtime = time.strftime("%Y%m%d%H%M%S", time.localtime())
 
-def deamon(soc=-1):
-  """ Main loop, gets battery data, gets summary.py to do logging"""
-  try:
-    import summary
-    logsummary = summary.Summary()
-    summary = logsummary.summary
-    printtime = time.strftime("%Y%m%d%H%M%S ", time.localtime())
-    while int(printtime) <= int(summary['current']['timestamp']):
-      print(printtime,summary['current']['timestamp'])
-      print ("Error: Current time before last sample time")
-      time.sleep(30)
-      printtime = time.strftime("%Y%m%d%H%M%S", time.localtime())
-    batdata = Readings()  # initialise batdata after we have valid sys time
-    alarms = Alarms(batdata,summary) # initialise alarms
+  print (str(printtime))
+  filecopy(config['files']['summaryfile'],config['files']['summaryfile']+"R" + str(int(printtime)))
+  if soc > config['battery']['capacity']:
+    print ("Battery DOD must be less than Battery Capacity")
 
-    print (str(printtime))
-    filecopy(config['files']['summaryfile'],config['files']['summaryfile']+"R" + str(int(printtime)))
-    if soc > config['battery']['capacity']:
-      print ("Battery DOD must be less than Battery Capacity")
-
+  else:
+    if soc < 0:
+       batdata.soc = summary['current']['ah'][0]
+       batdata.socadj = summary['current']['dod'][0]
     else:
-      if soc < 0:
-         batdata.soc = summary['current']['ah'][0]
-         batdata.socadj = summary['current']['dod'][0]
-      else:
-        batdata.soc = soc
-        batdata.socadj = soc
+      batdata.soc = soc
+      batdata.socadj = soc
+      summary['current']['dod'][3] = 0
+    summary['current']['dod'][3] = -100 # flag don't adjust leakage current
+    prevtime = logsummary.currenttime
+#    logsummary.startday(summary)
+#    logsummary.starthour(summary)
+
+def mainloop():
+  """ Main loop, gets battery data, gets summary.py to do logging"""
+
+  while True:
+    prevbatvoltage = batdata.batvoltsav[numcells]
+    for i in range(config['sampling']['samplesav']):
+#          printvoltage = ''
+#          for i in range(numcells+1):
+#            printvoltage = printvoltage + str(round(batdata.batvolts[i],3)).ljust(5,'0') + ' '
+#         print (printvoltage)
+      batdata.getraw()
+
+#          if batdata.batvoltsav[numcells] >= 55.2 and prevbatvoltage < 55.2:  # reset SOC counter?
+#          print batdata.socadj/(float(summary['current']['dod'][3])*24.0)
+      if batdata.batvoltsav[numcells] < config['battery']['vreset'] \
+      and prevbatvoltage >= config['battery']['vreset'] \
+      and summary['current']['dod'][3] != 0 \
+      and -batdata.currentav[0] < config['battery']['ireset']:  # reset SOC counter?
+
+        if summary['current']['dod'][3] <= 0 :
+          socerr=0
+        else:
+          socerr=batdata.socadj/(float(summary['current']['dod'][3])*24.0)
+          socerr=max(socerr,-0.01)
+          socerr=min(socerr,0.01)
+        config['battery']['ahloss']=config['battery']['ahloss']-socerr/2
+        batconfigdata=SafeConfigParser()
+        batconfigdata.read('battery.cfg')
+        batconfigdata.set('battery','ahloss',str(config['battery']['ahloss']))
+        with open('battery.cfg', 'w') as batconfig:
+          batconfigdata.write(batconfig)
+        batconfig.closed
+
+        batdata.soc = 0.0
+        batdata.socadj = 0.0
         summary['current']['dod'][3] = 0
-      summary['current']['dod'][3] = -100 # flag don't adjust leakage current
-      prevtime = logsummary.currenttime
-      prevbatvoltage = batdata.batvoltsav[numcells]
-  #    logsummary.startday(summary)
-  #    logsummary.starthour(summary)
+      else:
+        batdata.soc = batdata.soc + batdata.batah
+        batdata.socadj = batdata.socadj +batdata.batahadj
+      batdata.ah = batdata.ah + batdata.batah
+      batdata.inahtot = batdata.inahtot + batdata.inah
+      batdata.pwrbattot = batdata.pwrbattot + batdata.pwrbat
+      batdata.pwrintot = batdata.pwrintot + batdata.pwrin
+# check alarms
+    alarms.scanalarms(batdata)
+# update summaries
+    logsummary.update(summary, batdata)
+    if logsummary.currenttime[4] != logsummary.prevtime[4]:  # new minute
+      loadconfig()
+      batdata.pwravailable,batdata.minmaxdemandpwr=solaravailable(batdata)
+      logsummary.updatesection(summary, 'hour', 'current')
+      logsummary.updatesection(summary, 'alltime','current')
+      logsummary.updatesection(summary, 'currentday','current')
+      logsummary.updatesection(summary, 'monthtodate', 'current')
+      logsummary.updatesection(summary, 'yeartodate', 'current')
+      logsummary.writesummary()
+      batdata.ah = 0.0
+      batdata.ahadj = 0.0
+      batdata.inahtot = 0.0
+      batdata.pwrbattot = 0.0
+      batdata.pwrintot = 0.0
+      for i in range(batdata.numiins):
+        batdata.kWhin[i] = 0.0
+        batdata.kWhout[i] = 0.0
+      for i in range(numcells):
+        batdata.baltime[i]=0
 
 
-      while True:
-        try:
-          for i in range(config['sampling']['samplesav']):
-  #          printvoltage = ''
-  #          for i in range(numcells+1):
-  #            printvoltage = printvoltage + str(round(batdata.batvolts[i],3)).ljust(5,'0') + ' '
-  #         print (printvoltage)
-            batdata.getraw()
+    if logsummary.currenttime[3] != logsummary.prevtime[3]:  # new hour
+      logsummary.starthour(summary)
 
-  #          if batdata.batvoltsav[numcells] >= 55.2 and prevbatvoltage < 55.2:  # reset SOC counter?
-  #          print batdata.socadj/(float(summary['current']['dod'][3])*24.0)
-            if batdata.batvoltsav[numcells] < config['battery']['vreset'] \
-            and prevbatvoltage >= config['battery']['vreset'] \
-            and summary['current']['dod'][3] != 0 \
-            and -batdata.currentav[0] < config['battery']['ireset']:  # reset SOC counter?
+    if logsummary.currenttime[3] < logsummary.prevtime[3]: # newday
+      logsummary.startday(summary)
 
-              if summary['current']['dod'][3] <= 0 :
-                socerr=0
-              else:
-                socerr=batdata.socadj/(float(summary['current']['dod'][3])*24.0)
-                socerr=max(socerr,-0.01)
-                socerr=min(socerr,0.01)
-              config['battery']['ahloss']=config['battery']['ahloss']-socerr/2
-              batconfigdata=SafeConfigParser()
-              batconfigdata.read('battery.cfg')
-              batconfigdata.set('battery','ahloss',str(config['battery']['ahloss']))
-              with open('battery.cfg', 'w') as batconfig:
-                batconfigdata.write(batconfig)
-              batconfig.closed
+    if logsummary.currenttime[1] != logsummary.prevtime[1]: # new month
+      logsummary.startmonth(summary)
 
-              batdata.soc = 0.0
-              batdata.socadj = 0.0
-              summary['current']['dod'][3] = 0
-            else:
-              batdata.soc = batdata.soc + batdata.batah
-              batdata.socadj = batdata.socadj +batdata.batahadj
-            batdata.ah = batdata.ah + batdata.batah
-            batdata.inahtot = batdata.inahtot + batdata.inah
-            batdata.pwrbattot = batdata.pwrbattot + batdata.pwrbat
-            batdata.pwrintot = batdata.pwrintot + batdata.pwrin
-          prevbatvoltage = batdata.batvoltsav[numcells]
-  # check alarms
-          alarms.scanalarms(batdata)
-  # update summaries
-          logsummary.update(summary, batdata)
-          if logsummary.currenttime[4] != logsummary.prevtime[4]:  # new minute
-            loadconfig()
-            batdata.pwravailable,batdata.minmaxdemandpwr=solaravailable(batdata)
-            logsummary.updatesection(summary, 'hour', 'current')
-            logsummary.updatesection(summary, 'alltime','current')
-            logsummary.updatesection(summary, 'currentday','current')
-            logsummary.updatesection(summary, 'monthtodate', 'current')
-            logsummary.updatesection(summary, 'yeartodate', 'current')
-            logsummary.writesummary()
-            batdata.ah = 0.0
-            batdata.ahadj = 0.0
-            batdata.inahtot = 0.0
-            batdata.pwrbattot = 0.0
-            batdata.pwrintot = 0.0
-            for i in range(batdata.numiins):
-              batdata.kWhin[i] = 0.0
-              batdata.kWhout[i] = 0.0
-            for i in range(numcells):
-              batdata.baltime[i]=0
+    if logsummary.currenttime[0] != logsummary.prevtime[0]: # new year
+      logsummary.startyear(summary)
 
+import summary
+logsummary = summary.Summary()
+summary = logsummary.summary
+batdata = Readings()  # initialise batdata
+alarms = Alarms(batdata,summary) # initialise alarms
+def deamon(soc=-1):
+  """Battery Management deamon to run in background"""
 
-          if logsummary.currenttime[3] != logsummary.prevtime[3]:  # new hour
-            logsummary.starthour(summary)
-
-          if logsummary.currenttime[3] < logsummary.prevtime[3]: # newday
-            logsummary.startday(summary)
-
-          if logsummary.currenttime[1] != logsummary.prevtime[1]: # new month
-            logsummary.startmonth(summary)
-
-          if logsummary.currenttime[0] != logsummary.prevtime[0]: # new year
-            logsummary.startyear(summary)
-
-        except KeyboardInterrupt:
-          sys.stdout.write('\n')
-          logsummary.close()
-          sys.exit(9)
-          break
-  except Exception as err:
-    log.critical(err)
-    raise
+  for numtries in range(maxtries):
+    try:
+      initmain(soc)
+      mainloop()
+    except KeyboardInterrupt:
+      sys.stdout.write('\n')
+  #    sys.exit(9)
+      break
+    except Exception as err:
+      log.critical(err)
+      if numtries==maxtries-1:
+        raise
+    finally:
+      logsummary.close()
 
 if __name__ == "__main__":
   print (sys.argv)
